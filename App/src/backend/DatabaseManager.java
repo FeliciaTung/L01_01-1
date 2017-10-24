a8c3e0a36de7573796448178569a0cb06d983093
RenePiperi <renepiperi@hotmail.com>
2017-10-23 20:44:19 -0400

    Small addition to database insertion. Still rough, needs work.

--- App/src/backend/DatabaseManager.java
+++ App/src/backend/DatabaseManager.java
@@ -2,9 +2,26 @@ package backend;
 
 import holders.Question;
 
-public class DatabaseManager {
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+
+public class DatabaseManager {
+	
     public static void addQuestion(Question question) {
+    	try {
+    		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/C01ProjectDB");
+        	// SQL Query
+        	String sql = "INSERT INTO question(question) VALUES(?)";
+        	PreparedStatement pstmt = conn.prepareStatement(sql);
+        	pstmt.setString(1, question.question_string);
+        	pstmt.executeUpdate();
+    	} catch(SQLException e) {
+    		System.out.println(e.getMessage());
+    	}
 
         // If true we know the question is a multiple choice question
         if (question.multipleChoices != null) {
