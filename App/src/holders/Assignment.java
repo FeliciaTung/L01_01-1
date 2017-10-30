91f4600a71a434911452f18900524c4c3a57db1c
Adrian Ensan <adrianensan@gmail.com>
2017-10-30 17:53:53 -0400

    Add Assignment object

---
+++ App/src/holders/Assignment.java
@@ -0,0 +1,14 @@
+package holders;
+
+public class Assignment {
+
+    public String name;
+    public int courseID;
+    public int[] questions;
+
+    public Assignment(String name, int courseID, int[] questions) {
+        this.name = name;
+        this.courseID = courseID;
+        this.questions = questions;
+    }
+}
