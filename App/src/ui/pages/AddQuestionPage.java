df7bdcf51d425b12454aca57ca172b1fd3f8c072
talhautsc <talha.khatri@mail.utoronto.ca>
2017-11-23 22:48:38 -0500

    added 'Random' button to AddQuestionPage, to implement randomization on backend

--- App/src/ui/pages/AddQuestionPage.java
+++ App/src/ui/pages/AddQuestionPage.java
@@ -20,16 +20,21 @@ public class AddQuestionPage extends JPanel implements MouseListener {
 
     private Button saveButton;
     private Button randButton;
+    private JLabel typeQuestion;
     private InputField questionInput;
     private InputField answerInput = new InputField();
     private InputField[] multipleChoiceOptions = new InputField[4];
     private RadioButton[] multipleChoiceRadioButtons = new RadioButton[4];
     private RadioButton[] topMenuOptions;
+    private RadioButton[] randMenuOptions;
+    private RadioButton[] randButtonOptions;
     private int questionType = -1;
     private Button backButton;
     private int WINDOW_WIDTH = 800;
     private int WINDOW_HEIGHT = 680;
+    //private String[] randQ = {"Simple Math", "Intermediate Statistics Question", "Expert Statistics Question"};
     Random rand = new Random();
+    String[] randLabels = {"Simple Math", "Intermediate Statistics Question", "Expert Statistics Question"};
 
     public AddQuestionPage() {
         super();
@@ -37,7 +42,9 @@ public class AddQuestionPage extends JPanel implements MouseListener {
         randButton = new Button("Randomize");
         saveButton.addMouseListener(this);
         questionInput = new InputField();
-        topMenuOptions = new RadioButton[2];
+        topMenuOptions = new RadioButton[3];
+        randMenuOptions = new RadioButton[3];
+        randButtonOptions = new RadioButton[2];
         backButton = new Button("Back");
 
         setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
@@ -68,7 +75,7 @@ public class AddQuestionPage extends JPanel implements MouseListener {
 
         add(UIManager.getSpacing(WINDOW_WIDTH, 40));
 
-        JLabel typeQuestion = new JLabel("Question:", SwingConstants.RIGHT);
+        typeQuestion = new JLabel("Question:", SwingConstants.RIGHT);
         typeQuestion.setFont(getFont().deriveFont(18f));
         add(typeQuestion);
         add(questionInput);
@@ -83,14 +90,19 @@ public class AddQuestionPage extends JPanel implements MouseListener {
         if (typeChose) {
             if (questionType == ClickableObject.MC_BUTTON) {
                 addMultipleChoice();
-            } else {
+                add(UIManager.getSpacing(WINDOW_WIDTH, 30));
+                add(randButton);
+                add(saveButton);
+            } else if (questionType == ClickableObject.SA_BUTTON) {
                 addShortAnswer();
+                add(UIManager.getSpacing(WINDOW_WIDTH, 30));
+                add(randButton);
+                add(saveButton);
+            } else {
+                addRandom();
+                add(UIManager.getSpacing(WINDOW_WIDTH, 30));
+                add(saveButton);
             }
-            add(UIManager.getSpacing(WINDOW_WIDTH, 30));
-
-            add(randButton);
-            add(saveButton);
-
         }
         add(UIManager.getSpacing(WINDOW_WIDTH, 30));
         UIManager.switchView(this);
@@ -112,18 +124,20 @@ public class AddQuestionPage extends JPanel implements MouseListener {
             if (optionId == ClickableObject.MC_BUTTON) {
                 JLabel menuText = new JLabel("Multiple Choice", SwingConstants.LEFT);
                 menuText.setFont(getFont().deriveFont(16f));
-                menuText.setPreferredSize(new Dimension(InputField.WIDTH, 25));
+                menuText.setPreferredSize(new Dimension(InputField.WIDTH - 100, 25));
                 add(menuText);
-            } else {
+            } else if (optionId == ClickableObject.SA_BUTTON) {
                 JLabel menuText = new JLabel("Short Answer", SwingConstants.LEFT);
                 menuText.setFont(getFont().deriveFont(16f));
-                menuText.setPreferredSize(new Dimension(InputField.WIDTH, 25));
+                menuText.setPreferredSize(new Dimension(InputField.WIDTH - 100, 25));
+                add(menuText);
+            } else {
+                JLabel menuText = new JLabel("Random", SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                menuText.setPreferredSize(new Dimension(InputField.WIDTH - 100, 25));
                 add(menuText);
             }
         }
-
-
-
     }
 
     public void addMultipleChoice() {
@@ -165,10 +179,81 @@ public class AddQuestionPage extends JPanel implements MouseListener {
     }
 
     public void addShortAnswer() {
+        add(UIManager.getSpacing(100, 1));
+
         JLabel typeAnswer = new JLabel("Answer:", SwingConstants.RIGHT);
         typeAnswer.setFont(getFont().deriveFont(18f));
         add(typeAnswer);
         add(answerInput);
+    }
+
+    public void addRandom() {
+        remove(typeQuestion);
+        remove(questionInput);
+
+        JLabel typeAnswer = new JLabel("Select from the following options:", SwingConstants.RIGHT);
+        typeAnswer.setFont(getFont().deriveFont(16f));
+        JLabel chooseType = new JLabel("Would you like this question to be MC or SA?", SwingConstants.RIGHT);
+        chooseType.setFont(getFont().deriveFont(16f));
+
+        //JComboBox randList = new JComboBox(randQ);
+
+        add(typeAnswer);
+
+        for (int i = 0; i < randMenuOptions.length; i++) {
+            randMenuOptions[i] = new RadioButton(ClickableObject.RAND_OPTIONS[i]);
+            randMenuOptions[i].addMouseListener(this);
+            int optionId = randMenuOptions[i].getID();
+            // radio button resets to deselected when question type is updated
+            if (optionId == questionType) {
+                randMenuOptions[i].select();
+            }
+            add(randMenuOptions[i]);
+
+            if (optionId == ClickableObject.SIMPLE_MATH) {
+                JLabel menuText = new JLabel(randLabels[0], SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                add(menuText);
+            } else if (optionId == ClickableObject.STATS1) {
+                JLabel menuText = new JLabel(randLabels[1], SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                add(menuText);
+            } else {
+                JLabel menuText = new JLabel(randLabels[2], SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                add(menuText);
+            }
+
+            add(UIManager.getSpacing(WINDOW_WIDTH, 1));
+        }
+
+        //add(randList);
+        add(UIManager.getSpacing(WINDOW_WIDTH, 25));
+        add(chooseType);
+        add(UIManager.getSpacing(1, 25));
+
+        for (int i = 0; i < randButtonOptions.length; i++) {
+            randButtonOptions[i] = new RadioButton(ClickableObject.RAND_BUTTONS[i]);
+            randButtonOptions[i].addMouseListener(this);
+            int optionId = randButtonOptions[i].getID();
+            // radio button resets to deselected when question type is updated
+            if (optionId == questionType) {
+                randButtonOptions[i].select();
+            }
+            add(randButtonOptions[i]);
+
+            if (optionId == ClickableObject.RMC_BUTTON) {
+                JLabel menuText = new JLabel("Multiple Choice", SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                menuText.setPreferredSize(new Dimension(InputField.WIDTH - 135, 20));
+                add(menuText);
+            } else {
+                JLabel menuText = new JLabel("Short Answer", SwingConstants.LEFT);
+                menuText.setFont(getFont().deriveFont(16f));
+                menuText.setPreferredSize(new Dimension(InputField.WIDTH-100, 20));
+                add(menuText);
+            }
+        }
     }
 
     public void randomizeQuestion() {
@@ -226,14 +311,38 @@ public class AddQuestionPage extends JPanel implements MouseListener {
                     }
                 }
                 break;
+            case ClickableObject.SIMPLE_MATH:
+            case ClickableObject.STATS1:
+            case ClickableObject.STATS2:
+                for (RadioButton randMenu : randMenuOptions) {
+                    if (id == randMenu.getID()) {
+                        randMenu.select();
+                    } else {
+                        randMenu.deselect();
+                    }
+                }
+                break;
+            case ClickableObject.RMC_BUTTON:
+            case ClickableObject.RSA_BUTTON:
+                for (RadioButton randButton : randButtonOptions) {
+                    if (id == randButton.getID()) {
+                        randButton.select();
+                    } else {
+                        randButton.deselect();
+                    }
+                }
+                break;
             case ClickableObject.MC_BUTTON:
             case ClickableObject.SA_BUTTON:
+            case ClickableObject.RAND_OPTION:
                 for (RadioButton radioButton : topMenuOptions) {
                     if (id == radioButton.getID()) {
                         if (id == ClickableObject.MC_BUTTON) {
                             questionType = ClickableObject.MC_BUTTON;
-                        } else {
+                        } else if (id == ClickableObject.SA_BUTTON) {
                             questionType = ClickableObject.SA_BUTTON;
+                        } else {
+                            questionType = ClickableObject.RAND_OPTION;
                         }
                         addContent(true);
                     } else {
@@ -301,10 +410,24 @@ public class AddQuestionPage extends JPanel implements MouseListener {
                     answerChoices[correctAnswer == null ? i : i - 1] = multipleChoiceOptions[i].getText();
                 }
             }
-        } else {
+            DatabaseManager.addQuestion(new Question(question, correctAnswer, "", answerChoices));
+        } else if (questionType == ClickableObject.SA_BUTTON) {
             correctAnswer = answerInput.getText();
             answerChoices = null;
+            DatabaseManager.addQuestion(new Question(question, correctAnswer, "", answerChoices));
+        } else {
+            correctAnswer = "Correct answer will be randomized once assignment is generated";
+            for (int i = 0; i < randMenuOptions.length; i++) {
+                if (randMenuOptions[i].isSelected()) {
+                    question = randLabels[i];
+                }
+            }
+            if (randButtonOptions[0].isSelected()) {
+                answerChoices[0] = "0"; answerChoices[1] = "0"; answerChoices[2] = "0";
+            } else {
+                answerChoices = null;
+            }
+            DatabaseManager.addQuestion(new Question(question, correctAnswer, "r", answerChoices));
         }
-        DatabaseManager.addQuestion(new Question(question, correctAnswer, "", answerChoices));
     }
 }
